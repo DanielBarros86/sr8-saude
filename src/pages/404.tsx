a9aa1ffeb7ec9a7f7cfeb207d70368c40bbfdb6c
Lucas Almeida da Silva <lucas_almeidasilva01@hotmail.com>
2022-07-11 13:48:29 -0300

    feat: add SEO on 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,11 @@
+import { SEO } from '../components/SEO';
 import { Container } from '../styles/pages/_404';
 
 export default function PageNotFound() {
   return (
     <Container>
+      <SEO title="SR8 Saúde Brasil" shouldIndexPage={false} />
+
       <strong>404</strong>
       <strong>Página não encontrada</strong>
     </Container>
